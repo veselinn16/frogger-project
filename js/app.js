37df826d7dbab9e3635e7494ad25411d1b2f4228
veselin <veselinn16@yahoo.com>
2018-06-03 12:45:11 +0300

    Hanlde collissions

--- js/app.js
+++ js/app.js
@@ -9,11 +9,13 @@ class Enemy {
     this.sprite = 'images/enemy-bug.png';
     this.x = 0;
     this.ranNumForLocation = Math.floor(Math.random() * 3);
-    this.initialLocationArray = [60, 145, 228]
+    this.initialLocationArray = [48, 131, 214]
     this.y = this.initialLocationArray[this.ranNumForLocation];
     this.ranNumForSpeed = Math.floor(Math.random() * 8);
     this.speedArray = [100, 150, 200, 250, 300, 350, 400, 450, 500];
     this.speed = this.speedArray[this.ranNumForSpeed];
+    this.row = null;
+    this.col = 1;
   }
   // Update the enemy's position, required method for game
   // Parameter: dt, a time delta between ticks
@@ -23,11 +25,41 @@ class Enemy {
     // all computers.
 
     this.x = this.x + this.speed * dt;
-    // if(this.x - Player.x) {
-    //   Player.reset()
-    //   allEnemies = [];
-    // }
-    // console.log(Player.x);
+
+    if(this.x < 102) {
+      this.col = 1;
+    } else if(this.x < 203) {
+      this.col = 2;
+    } else if(this.x < 304) {
+      this.col = 3;
+    } else if(this.x < 405) {
+      this.col = 4;
+    } else if(this.x < 505) {
+      this.col = 5;
+    } else if(this.x < 550) {
+      this.col = 7;
+    }
+
+
+    switch(this.y) {
+      case 48:
+        this.row = 2;
+      break;
+      case 131:
+        this.row = 3;
+      break;
+      case 214:
+        this.row = 4;
+    }
+
+    for(const enemy of allEnemies) {
+      // console.log(`   row is ${enemy.row}`);
+      // console.log(`col is ${enemy.col}`);
+      if (enemy.col === player.col && enemy.row === player.row) {
+        player.reset();
+        allEnemies = [];
+      }
+    }
   }
   // Draw the enemy on the screen, required method for game
   render() {
@@ -43,9 +75,11 @@ class Player {
         this.sprite = "images/char-boy.png";
         this.x = 200;
         this.y = 380;
+        this.row = 6;
+        this.col = 3;
     }
     update(dt) {
-      if(this.y === -20) {
+      if(this.row === 1) {
         this.reset();
         allEnemies = [];
       }
@@ -57,28 +91,34 @@ class Player {
       switch (allowedKeys) {
         case "up":
           if (this.y > -20) {
-            this.y = this.y - 80;
+            this.y = this.y - 83;
+            this.row -= 1;
           }
         break;
         case "down":
         if (this.y < 380) {
-          this.y = this.y + 80;
+          this.y = this.y + 83;
+          this.row += 1;
         }
         break;
         case "left":
-          if (this.x > -4) {
-            this.x = this.x - 102;
+          if (this.x > 4) {
+            this.x = this.x - 101;
+            this.col -= 1;
           }
         break;
         case 'right':
-          if (this.x < 404) {
-            this.x = this.x + 102;
+          if (this.x < 396) {
+            this.x = this.x + 101;
+            this.col += 1;
           }
       }
     }
     reset() {
       this.x = 200;
-      this.y = 380; 
+      this.y = 380;
+      this.row = 6;
+      this.col = 3; 
     }
 }
 
@@ -88,6 +128,8 @@ class Player {
 const player = new Player();
 
 let allEnemies = [];
+// let enemy = new Enemy();
+// allEnemies.push(enemy);
 
 setInterval(function() { 
   let enemy = new Enemy();
